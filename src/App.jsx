28498e0b523744148e24d2c0af2a55c32bb3cef0
ali <alouloucheikh05@gmail.com>
2020-08-08 15:10:06 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header"
+import myImage from './img1.png'
+
+// import Header from "./components/Header"
+
 
 function App() {
   return (
- 
-    <div className="App1">
-       <h3>welcam to my web site</h3>
-      <form className="App2">
-      <input type="text" class="form-control" className="App1" placeholder="First name" />
-      <input type="text" class="form-control" className="App1" placeholder="Last name" />
+    <div className="hello-ahmed">
+      <h1 style={{
+        Border:'solid black',
+        maxWidth:'100vw',
+        color:'red'
+         }}hi>
+     hi how are you fine go ...  </h1>
+      <img src="/img3.png" alt="#" className="img" /><br />
+      <img src={myImage} alt="#" className="img"  /><br />
+      <h3>video link</h3>
       <br/>
-      <img src="/img/logo512.png" alt="#"className="img" />
-      <br />
-      <br />
-      <submit className="btn btn-primary">submit</submit>
-      </form>
+      <h4>https://www.youtube.com/watch?v=9AboneIxeM8&list=PL3KAvm6JMiowqFTXj3oPQkhP7aCgRHFTm&index=1&ab_channel=PaulHanna</h4>
+      <video width="1000" height="700" controls><source src="/hi.mp4" type="video/mp4" /> </video>
+   <br/>
+   
+   
     </div>
   );
 }
